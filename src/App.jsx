207ff8c9d9c4c07b9c84ae6cb2a8a700a0214f70
Geoffrey Kithuku <mutembeikithuku12@gmail.com>
2023-08-10 21:59:27 +0300

    setup pages and react-router

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { Routes, Route } from "react-router-dom";
+import Explore from "./pages/Explore";
+import Offers from "./pages/Offers";
+import ForgotPassword from "./pages/ForgotPassword";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Profile from './pages/Profile'
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Explore />} />
+        <Route path="/offers" element={<Offers />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/reset" element={<ForgotPassword />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
