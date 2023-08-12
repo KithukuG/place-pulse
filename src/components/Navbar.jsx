a42b68d955d8b81956fb738076a34e7ce4cdbf66
Geoffrey Kithuku <mutembeikithuku12@gmail.com>
2023-08-12 21:28:47 +0300

    login form

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,6 +4,8 @@ import { useNavigate, useLocation } from "react-router-dom";
 import { ReactComponent as PersonIcon } from "../assets/svg/personOutlineIcon.svg";
 import { ReactComponent as ExploreIcon } from "../assets/svg/exploreIcon.svg";
 import { ReactComponent as OfferIcon } from "../assets/svg/localOfferIcon.svg";
+
+
 const Navbar = () => {
   const navigate = useNavigate();
   const location = useLocation();
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,58 @@
-import React from 'react'
+import React from "react";
+
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { ReactComponent as ArrowRightIcon } from "../assets/svg/keyboardArrowRightIcon.svg";
+import visibilityIcon from "../assets/svg/visibilityIcon.svg";
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  const [showPassword, setShowPassword] = useState(false);
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+  const navigate = useNavigate();
 
-export default Login
+  function handleChange(e) {
+    e.preventDefault();
+  }
+
+  return (
+    <div className="pageContainer">
+      <header>
+        <p className="pageHeader">Welcome back</p>
+      </header>
+      <form>
+        <input
+          type="email"
+          placeholder="Email"
+          value={formData.email}
+          className="emailInput"
+          id="email"
+          onChange={handleChange}
+        />
+        <div className="passwordInputDiv">
+          <input
+            type={showPassword ? "password" : "text"}
+            placeholder="Password"
+            id="password"
+            value={formData.password}
+            onChange={handleChange}
+            className="passwordInput"
+          />
+          <img
+            src={visibilityIcon}
+            alt="show password"
+            className="showPassword"
+            onClick={() => setShowPassword(!showPassword)}
+          />
+        </div>
+        <Link to="/forgot-password" className="forgotPasswordLink">
+          Forgot Password
+        </Link>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
