47008ffc86be0eae1f5eeceae6f133466c3b571b
Geoffrey Kithuku <mutembeikithuku12@gmail.com>
2023-09-21 22:37:24 +0300

    fix profile: remove css

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -3,55 +3,36 @@ import React, { useEffect, useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
 
 import { toast } from "react-toastify";
-import arrowRight from "../assets/svg/keyboardArrowRightIcon.svg";
+
 import homeIcon from "../assets/svg/homeIcon.svg";
 
 const Profile = () => {
-  const auth = getAuth();
   const [changeDetails, setChangeDetails] = useState(false);
   const [formData, setFormData] = useState({
-    name: auth.currentUser.displayName,
-    email: auth.currentUser.email,
+    name: "",
+    email: "",
   });
   const navigate = useNavigate();
   const { name, email } = formData;
-  function onLogout() {
-    auth.signOut();
-    navigate("/");
-  }
+  function onLogout() {}
   function onChange(e) {
     setFormData((prev) => ({ ...prev, [e.target.id]: e.target.value }));
   }
-  async function onSubmit() {
-    try {
-      if (auth.currentUser.displayName !== name) {
-        await updateProfile(auth.currentUser, {
-          displayName: name,
-        });
-
-        const userRef = doc(db, "users", auth.currentUser.uid);
-        await updateDoc(userRef, {
-          name,
-        });
-      }
-    } catch (e) {
-      toast.error("Error occurred!");
-    }
-  }
+  async function onSubmit() {}
   return (
-    <div className="profile">
-      <header className="profileHeader">
-        <p className="pageHeader">My profile</p>
-        <button onClick={onLogout} type="button" className="logOut">
+    <div className="">
+      <header className="">
+        <p className="">My profile</p>
+        <button onClick={onLogout} type="button" className="">
           Log out
         </button>
       </header>
 
       <main>
-        <div className="profileDetailsHeader">
-          <p className="profileDetailsText">Personal Details</p>
+        <div className="">
+          <p className="">Personal Details</p>
           <p
-            className="changePersonalDetails"
+            className=""
             onClick={() => {
               changeDetails && onSubmit();
               setChangeDetails((prev) => !prev);
@@ -60,7 +41,7 @@ const Profile = () => {
             {changeDetails ? "done" : "change"}
           </p>
         </div>
-        <div className="profileCard">
+        <div className="">
           <form>
             <input
               disabled={!changeDetails}
@@ -81,7 +62,7 @@ const Profile = () => {
           </form>
         </div>
 
-        <Link to="/create" className="createListing">
+        <Link to="/create" className="">
           <img src={homeIcon} alt="" />
           <p>Sell or rent your home</p>
         </Link>
